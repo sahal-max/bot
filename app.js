d686fd50bb62385764bc6594b639111e9cffd11f
sahal-max <pnel31275@gmail.com>
2026-05-30 18:01:37 +0700

    fix: nama store di tengah dengan garis atas dan bawah

--- app.js
+++ app.js
@@ -3965,7 +3965,8 @@ async function sendMainMenu(ctx) {
     });
   } catch (e) {}
 
-  const messageText = `✦ <b>${NAMA_STORE}</b> ✦
+  const messageText = `<code>──────────────────────</code>
+      ✦ <b>${NAMA_STORE}</b> ✦
 <code>──────────────────────</code>
 👤 <b>Informasi Pengguna</b>
 <code>──────────────────────</code>
